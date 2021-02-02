06c5919d1748e63cd109e674849663c472857e03
Tim Schmidt <ts@devtim.de>
2021-02-02 10:59:19 +0100

    stuff for testing applepay

--- functions/applepay-checkout/applepay-checkout.js
+++ functions/applepay-checkout/applepay-checkout.js
@@ -1,33 +1,36 @@
-const axios = require("axios");
+const axios = require("axios")
 
 exports.handler = async function (event) {
-  const { validationURL } = JSON.parse(event.body)
+  try {
+    console.log("test")
+    // const { validationURL } = JSON.parse(event.body)
 
-  // ttpsAgent = new https.Agent({
-  //   rejectUnauthorized: false,
-  //   cert: fs.readFileSync(path.join(__dirname, "./certificate.pem")),
-  //   key: fs.readFileSync(path.join(__dirname, "./sandbox.key")),
-  // })
+    // ttpsAgent = new https.Agent({
+    //   rejectUnauthorized: false,
+    //   cert: fs.readFileSync(path.join(__dirname, "./certificate.pem")),
+    //   key: fs.readFileSync(path.join(__dirname, "./sandbox.key")),
+    // })
 
-  // const result = await axios
-  //   .post(
-  //     "validationURL",
-  //     {
-  //       merchantIdentifier: "merchant.com.example.mystore",
-  //       displayName: "MyStore",
-  //       initiative: "web",
-  //       initiativeContext: "mystore.example.com",
-  //     },
-  //     {
-  //       httpsAgent,
-  //     }
-  //   );
+    // const result = await axios
+    //   .post(
+    //     "validationURL",
+    //     {
+    //       merchantIdentifier: "merchant.com.example.mystore",
+    //       displayName: "MyStore",
+    //       initiative: "web",
+    //       initiativeContext: "mystore.example.com",
+    //     },
+    //     {
+    //       httpsAgent,
+    //     }
+    //   );
 
     const result = await axios.post(
       "https://api.mollie.com/v2/wallets/applepay/sessions",
       {
         domain: "devtimshop.netlify.app",
-        validationURL,
+        validationURL:
+          "https://apple-pay-gateway.apple.com/paymentservices/startSession",
       },
       {
         headers: {
@@ -36,8 +39,11 @@ exports.handler = async function (event) {
       }
     )
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    }
+  } catch (e) {
+    console.log(e)
   }
 }
